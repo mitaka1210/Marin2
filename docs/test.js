fc74e79253a57832d9a741ef25f41762db71caf0
Mitaka <dimitar_dimitrov12@mail.bg>
2019-10-08 19:32:50 +0300

    change style

--- docs/test.js
+++ docs/test.js
@@ -1,19 +1,22 @@
-$('li.nav-about').click(function () {
-    $(this).find('ul').toggle();
-    $(this).siblings('li').find('ul').hide();
-});
+<script type="text/javascript">
+  // Menu toggle-button
+    $(document).ready(function(){
+        $(".menu-icon").on("click", function(){
+            $("nav ul").toggleClass("showing");
+        })
+    });
 
-$(' li.nav-industry').click(function () {
-    $("html, body").animate({
-        scrollTop: 0
-    }, "fast");
-    $(this).find('ul').toggle();
-    $(this).siblings('li').find('ul').hide();
-});
-$('li.nav-application').click(function () {
-    $("html, body").animate({
-        scrollTop: 0
-    }, "fast");
-    $(this).find('ul').toggle();
-    $(this).siblings('li').find('ul').hide();
-});
+
+  //scroling effect
+
+  $(window).on("scroll", function(){
+
+    if($(window).scrollTop()) {
+      $('nav').addClass('black')
+
+    }
+    else {
+        $('nav').removeClass('black')
+    }
+  });
+</script>
